482590ddd5f68f139850ff7928e38bdc30f2425e
Rui Zhao <rzhao@Ruis-MBP.T-mobile.com>
2022-12-23 13:23:47 -0500

    return expected json

--- server/index.ts
+++ server/index.ts
@@ -8,12 +8,29 @@ const port = process.env.SERVER_PORT;
 
 app.get('/getUserInfo', async (req: Request, res: Response) => {
     const username = 'mavrckco'
-    const test = await axios.get(`${process.env.INSTAGRAM_URL}api/v1/users/web_profile_info/?username=${username}`, {
+    const rawResult = await axios.get(`${process.env.INSTAGRAM_URL}api/v1/users/web_profile_info/?username=${username}`, {
       headers: {
         'User-Agent': 'Instagram 219.0.0.12.117 Android'
       }
     })
-   const result = await test.data.data
+   const user = await rawResult.data.data.user
+   const edges = user.edge_owner_to_timeline_media?.edges
+   const posts = edges.map((edge:any)=>{
+    const node = edge?.node
+    const post = {
+        mediaUrl: node.display_url,
+        postType: node.__typename,
+        numberOfLikes: node.edge_liked_by?.count || 0,
+        numberOfComments: node.edge_media_to_comment?.count || 0
+    }
+   })
+
+   const result = {
+    biography: user.biography,
+    fullName: user.full_name,
+    followerCount: user.edge_followed_by?.count,
+    posts
+   }
   res.send(result)
 });
 
