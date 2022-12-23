0467c3e26fb203f7a47443078fc2e3369e9fdf5e
Rui Zhao <rzhao@Ruis-MBP.T-mobile.com>
2022-12-23 12:41:43 -0500

    backend call to instagram

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,20 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import axios from 'axios'
 
 dotenv.config()
 const app: Express = express()
 const port = process.env.SERVER_PORT;
 
-app.get('/getUserInfo', (req: Request, res: Response) => {
-  res.send({ message: "Hello from server!" })
+app.get('/getUserInfo', async (req: Request, res: Response) => {
+    const username = 'mavrckco'
+    const test = await axios.get(`${process.env.INSTAGRAM_URL}api/v1/users/web_profile_info/?username=${username}`, {
+      headers: {
+        'User-Agent': 'Instagram 219.0.0.12.117 Android'
+      }
+    })
+   const result = await test.data.data
+  res.send(result)
 });
 
 app.listen(port, () => {
