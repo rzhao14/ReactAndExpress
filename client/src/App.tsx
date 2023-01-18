e82ff9b5771b8d4baf34fc932f567d41367a7e3d
Rui Zhao <rzhao@Ruis-MBP.T-mobile.com>
2023-01-18 13:32:19 -0500

    tslint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,68 @@
-import React, { useCallback, useState } from 'react';
-import './App.css';
-import UserInfo from './components/UserInfo';
+import React, { useCallback, useState } from 'react'
+import './App.css'
+import UserInfo from './components/UserInfo'
 
 function App() {
-  const [data, setData] = useState<any>(null);
-  const [handle, setHandle] = useState('');
-  const [forceUpdate, setForceUpdate] = useState(false);
+  const [data, setData] = useState<any>(null)
+  const [handle, setHandle] = useState('')
+  const [forceUpdate, setForceUpdate] = useState(false)
 
   const handleSubmit = useCallback(() => {
-      if(handle && handle!==''){
-          fetch(`/getUserInfo?handle=${handle}&force=${forceUpdate}`)
-            .then((res) => res.json())
-            .then((data) => setData(data))
-      }
-  }, [handle, forceUpdate])
+    if (handle && handle !== '') {
+      fetch(`/getUserInfo?handle=${handle}&force=${forceUpdate}`)
+        .then((res) => res.json())
+        .then((json) => setData(json))
+    }
+  }, [handle, forceUpdate, setData])
 
   const handleClear = useCallback(() => {
     setData(null)
-}, [])
+  }, [])
 
-  const handleSelect = (e:React.ChangeEvent<HTMLSelectElement>) => {
-     setData(null)
-     setHandle(e.target.value)
- }
- const handleChange = () => {
-  setForceUpdate(!forceUpdate);
-};
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setData(null)
+    setHandle(e.target.value)
+  }
+  const handleChange = () => {
+    setForceUpdate(!forceUpdate)
+  }
 
-  const handles = [
-    'mavrckco',
-    'Otanikotani',
-    'haiyang-mavrck',
-    'imclarney',
-    'MattGirolami'
-  ]
+  const handles = ['mavrckco', 'Otanikotani', 'haiyang-mavrck', 'imclarney', 'MattGirolami']
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Get user Info from Instagram</p>
       </header>
-        <div>
-         Please choose Instagram handle:
-             <select onChange={handleSelect}>
-             <option value='' key='empty'>--</option>
-                {handles.map((handle)=>{
-                  return <option value={handle} key={handle}>{handle}</option>
-                })}
-             </select>
-         or type:
-         <input type="text" name="handle" onChange={(e) => setHandle(e.target.value)} value={handle}/>
-         <input type="submit" value="Get" onClick={handleSubmit} />
-         <input type="submit" value="clear" onClick={handleClear} />
-         Force Update?
-         <input type="checkbox"           
-                checked={forceUpdate}
-                onChange={handleChange} />
-        </div>
-        {data && <UserInfo data={data}/>}
+      <div>
+        Please choose Instagram handle:
+        <select onChange={handleSelect}>
+          <option value="" key="empty">
+            --
+          </option>
+          {handles.map((userHandle) => {
+            return (
+              <option value={userHandle} key={userHandle}>
+                {userHandle}
+              </option>
+            )
+          })}
+        </select>
+        or type:
+        <input
+          type="text"
+          name="handle"
+          onChange={(e) => setHandle(e.target.value)}
+          value={handle}
+        />
+        <input type="submit" value="Get" onClick={handleSubmit} />
+        <input type="submit" value="clear" onClick={handleClear} />
+        Force Update?
+        <input type="checkbox" checked={forceUpdate} onChange={handleChange} />
+      </div>
+      {data && <UserInfo data={data} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -1,12 +1,12 @@
-var request = require('supertest')
-var app = require("../index")
+import request = require('supertest')
+import app = require('../index')
 
 describe('testing index file', () => {
   test('get mavrckco should return 200', async () => {
-    const res = await request(app).get("/getUserInfo?handle=mavrckco");
-    expect(res.statusCode).toBe(200);
-    expect(res.body.fullName).toBe('Mavrck');
+    const res = await request(app).get('/getUserInfo?handle=mavrckco')
+    expect(res.statusCode).toBe(200)
+    expect(res.body.fullName).toBe('Mavrck')
     // The test can fail because of rate limit exceed when call to instagram
-    //expect(res.body.error).toBe('error fetching user info');
-  });
-});
+    // expect(res.body.error).toBe('error fetching user info');
+  })
+})
